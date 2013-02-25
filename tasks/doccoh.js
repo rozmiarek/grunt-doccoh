847d90f4444395343891037d7dae40f152ab5745
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-02-25 13:45:24 +0000

    updated to use new way of fetching options

--- tasks/doccoh.js
+++ tasks/doccoh.js
@@ -12,11 +12,10 @@ module.exports = function (grunt) {
   grunt.registerMultiTask('doccoh', 'Doccoh processor.', function() {
 
     var done = this.async();
-    var helpers = require('grunt-lib-contrib').init(grunt);
-    var options = helpers.options(this);
+    var options = grunt.config.get('doccoh');
 
     grunt.verbose.writeflags(options, "Options");
-    var src = grunt.file.expand(this.file.src);
+    var src = grunt.file.expand(options.src);
 
     docco.document(src, options, function(err, result, code){
       grunt.log.writeln("Doccoed [" + src.join(", ") + "]; " + err ? err : "(No errors)" + "\n" + result + " " + code);
